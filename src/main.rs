642795d37a514f82fb4b05fc44a8d276d94ee191
Chris Flinn <christopher.flinn@gmail.com>
2026-04-09 13:55:17 +1000

    zerocopy

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::io::Write;
+use zerocopy::{
+    Immutable, IntoBytes,
+    little_endian::{U16, U32},
+};
 
+#[derive(IntoBytes, Immutable)]
 #[repr(u16)]
 enum WaveFormatCategory {
     /// Microsoft Pulse Code Modulation (PCM) format
-    Pcm = 0x0001,
+    Pcm = 0x0001u16.to_le(),
 }
 
+#[derive(IntoBytes, Immutable)]
 #[repr(C, packed)]
 struct FormatChunkCommon {
-    format_tag: u16,
-    channels: u16,
-    samples_per_sec: u32,
-    avg_bytes_per_sec: u32,
-    block_align: u16,
+    format_tag: WaveFormatCategory,
+    channels: U16,
+    samples_per_sec: U32,
+    avg_bytes_per_sec: U32,
+    block_align: U16,
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -22,35 +28,13 @@ fn main() -> Result<(), std::io::Error> {
     out.write_all(b"WAVE")?;
     out.write_all(b"fmt ")?;
     out.write_all(&(std::mem::size_of::<FormatChunkCommon>() as u32).to_le_bytes())?;
-    out.write_all(unsafe {
-        cast_to_bytes(&FormatChunkCommon {
-            format_tag: WaveFormatCategory::Pcm as u16,
-            channels: 1,
-            samples_per_sec: 44100,
-            avg_bytes_per_sec: 2 * 44100,
-            block_align: 64,
-        })
-    })?;
-    out.write_all(&0u32.to_le_bytes())?; // wave chunk
+    FormatChunkCommon {
+        format_tag: WaveFormatCategory::Pcm,
+        channels: 1.into(),
+        samples_per_sec: 44100.into(),
+        avg_bytes_per_sec: (2 * 44100).into(),
+        block_align: 64.into(),
+    }
+    .write_to_io(&mut out)?;
     Ok(())
-}
-
-/// # Safety
-///
-/// Only safe to call on packed types. If called on unpacked types, this
-/// would allow you to read padding bytes, which may be uninitialized memory
-// cast_to_bytes takes a reference to a T, and gives you back a slice of the bytes
-// of that type.
-unsafe fn cast_to_bytes<T>(t: &T) -> &[u8] {
-    // cast t to a raw pointer to a T, then cast that to a raw pointer to a u8.
-    // so now it is a pointer to the same place, of type byte instead of type T
-    let ptr = t as *const T as *const u8;
-    // create a slice of type ptr (u8), pass size of T (number of bytes of data)
-    // returned unsafely as a slice
-    // Safety:
-    //
-    // ptr is valid until ptr + sizeof<T> since it _was_ a &T which is valid for sizeof<T>
-    // ptr to ptr+sizeof is valid since we had a &T
-    // ptr to ptr+sizeof contains initialized bytes by the safety requirement of this function
-    unsafe { std::slice::from_raw_parts(ptr, std::mem::size_of::<T>()) }
 }
